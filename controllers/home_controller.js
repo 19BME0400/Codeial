568af9f76c106430b18263911a5b6de1b746f441
19BME0400 <shailesh.kschauhan@gmail.com>
2024-05-05 03:04:10 +0530

    Post form with authentication

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,15 @@
-module.exports.home = function(req, res){
-    console.log(req.cookies);
-    res.cookie('user_id', 25)
+const Post = require('../models/post');
+
+module.exports.home = async function(req, res){
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+    
+    
+    // populate the user of each post
+    let posts = await Post.find({}).populate('user').exec()
     return res.render('home', {
-        title: "Home"
+        title: "Codeial | Home",
+        posts: posts
     });
 }
 
